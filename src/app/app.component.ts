7f742ab4c96f5c81fa7948c1506dc4fffb2dd9a7
unknown <jmbevents2@gmail.com>
2020-07-30 01:00:07 +0100

    dropdowns to follow parent back colour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ViewEncapsulation, OnDestroy } from '@angular/core';
 import { UiStateService } from './services/ui-state.service';
 import { Subscription } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
@@ -9,26 +10,44 @@ import { Subscription } from 'rxjs';
 })
 export class AppComponent implements OnInit, OnDestroy {
 
-
   appUI;
-
   app;
-  sub1: Subscription;
+  subs: Subscription[] = []; // a cache of subscriptions
+  smMenuVertical;
 
-  constructor(private uiState: UiStateService) {
-    this.sub1 = this.uiState.getState().subscribe(state => {
-      this.app = state.app;
-      console.log(state);
-    }
+  constructor(
+    private uiState: UiStateService,
+    private breakpointObserver: BreakpointObserver) {
+    this.subs.push(
+      this.uiState.getState().subscribe(state => {
+        this.app = state.app;
+        console.log(state);
+      }
+      )
     );
-  }
-
-  ngOnDestroy() {
-    this.sub1.unsubscribe();
   }
 
   ngOnInit(): void {
 
+    // 844px appears to match  navbar-expand-md
+    this.subs.push(
+      this.breakpointObserver.observe([
+        '(max-width: 844px)'
+      ]).subscribe(result => {
+        if (result.matches) {
+          console.log('<=844px');
+          this.smMenuVertical = 'collapse';
+        } else {
+          // if necessary:
+          console.log('>844px');
+          this.smMenuVertical = 'undefined';
+        }
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subs.forEach(sub => sub.unsubscribe());
   }
 
   implementMe(x: string) {
